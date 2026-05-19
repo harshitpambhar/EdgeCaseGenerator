e22e09620838763121f74ff6311a1bd10a86d646
Tirth-22 <tirthmakadia2006@gmail.com>
2026-05-19 12:19:52 +0530

    4v - solve feign client error and set @Configuration as path

--- backend/auth-service/src/main/java/com/microservices/auth_service/client/UserServiceFeignConfig.java
+++ backend/auth-service/src/main/java/com/microservices/auth_service/client/UserServiceFeignConfig.java
@@ -3,7 +3,9 @@ package com.microservices.auth_service.client;
 import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class UserServiceFeignConfig {
 
     @Bean
